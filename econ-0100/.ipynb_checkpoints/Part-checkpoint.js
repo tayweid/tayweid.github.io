9970bd63e96ed9432bf8e3ed1652eb9f0a7bf2fb
Taylor J Weidman <taylorjweidman@gmail.com>
2025-06-03 01:05:41 -0400

    directory name change

---
+++ econ-0100/.ipynb_checkpoints/Part-checkpoint.js
@@ -0,0 +1,18 @@
+$(window).scroll(function(){
+    var scrollTop = $(document).scrollTop();
+    var anchors = $('body').find('div');
+    
+    for (var i = 0; i < anchors.length; i++){
+        if (scrollTop > $(anchors[i]).offset().top - 100 && scrollTop < $(anchors[i]).offset().top + $(anchors[i]).height() - 100) {
+            $('nav a[href="#' + $(anchors[i]).attr('id') + '"]').addClass('active');
+        } else {
+            $('nav a[href="#' + $(anchors[i]).attr('id') + '"]').removeClass('active');
+        }
+    }
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+}
