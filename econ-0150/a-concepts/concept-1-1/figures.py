06cb9f5c674da4c4c0e1fd96343908f2a5cccafe
Taylor J Weidman <taylorjweidman@gmail.com>
2025-08-19 23:36:41 -0400

    some content changes

---
+++ econ-0150/a-concepts/concept-1-1/figures.py
@@ -0,0 +1,50 @@
+# plot_config.py
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+from datetime import datetime, timedelta
+import numpy as np
+
+# Set Seaborn style
+sns.set_style('whitegrid')
+
+# Update Matplotlib parameters
+plt.rcParams.update({
+    'font.family': 'serif',              # Set the font family
+    'font.serif': ['Times New Roman'],   # Use a specific serif font
+    'font.size': 16,                     # Set the general font size
+    'axes.titlesize': 16,                # Set the font size of the title
+    'axes.labelsize': 16,                # Set the font size of axis labels
+    'font.style': 'italic',              # Set the font style to italic
+    'figure.dpi': 400   
+})
+category_color_pal = sns.color_palette("hls", 8)#sns.color_palette("husl", 8)
+continuous_color_pal = sns.color_palette("rocket_r", as_cmap=True)
+
+def Weid_Fig(rows=1, widths=10, height=6, dpi=400, fontsize=10):
+    # Set only title and label font sizes
+    plt.rcParams.update({
+        'axes.titlesize': fontsize,
+        'axes.labelsize': fontsize
+    })
+    
+    total_width = sum([w for w in widths])
+    if type(widths) == list:
+        widths = [w/total_width for w in widths]
+        cols = len(widths)
+    if type(widths) in [int, int]:
+        cols = 1
+    
+    fig, axes = plt.subplots(
+        rows, 
+        cols, 
+        figsize=(widths, height), 
+        dpi=dpi,
+        gridspec_kw={'width_ratios': [widths]}
+        )
+    
+    return fig, axes
